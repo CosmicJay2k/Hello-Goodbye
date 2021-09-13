6725427e9cdb9318aa0db7317fde0624a7f72847
CosmicJay2k <89969779+CosmicJay2k@users.noreply.github.com>
2021-09-13 14:34:57 +0200

    Switched from "onclick" to "event listener"

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
-function changeText() {
-    if (document.querySelector("#helloBye").innerHTML == "Hello World") {
-        document.querySelector("#helloBye").innerHTML = "Goodbye";
+let changeBtn = document.querySelector("#btn");
+
+changeBtn.addEventListener("click",
+    function () {
+        let btnText = document.querySelector("#helloBye").innerHTML
+        if (btnText == "Hello World") {
+            document.querySelector("#helloBye").innerHTML = "Goodbye";
+        }
+        else {
+            document.querySelector("#helloBye").innerHTML = "Hello World";
+
+        }
     }
-    else {
-        document.querySelector("#helloBye").innerHTML = "Hello World";
-    }
-}
+);
 /* JSPI21 - Uppgift 3 - Introduktion Till Webbutveckling - John - Tannenberg */
