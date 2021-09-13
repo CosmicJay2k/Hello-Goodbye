1b3c21bd381d69431247d907e296a9057dee1e46
CosmicJay2k <89969779+CosmicJay2k@users.noreply.github.com>
2021-09-13 13:34:19 +0200

    Formatting

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-function changeText(){
-    if (document.querySelector("#helloBye").innerHTML == "Hello World"){
+function changeText() {
+    if (document.querySelector("#helloBye").innerHTML == "Hello World") {
         document.querySelector("#helloBye").innerHTML = "Goodbye";
     }
-    else{
+    else {
         document.querySelector("#helloBye").innerHTML = "Hello World";
     }
 }
